7e815af33f88d6e4907d503dd711238f8865d9bf
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-11-07 20:07:04 +0100

    Update check_update.sh

--- check_update.sh
+++ check_update.sh
@@ -2,20 +2,18 @@
 PACKAGES=$(/usr/bin/checkupdates | awk '{print $1}')
 AURPACKAGES=$(/usr/bin/yay -Qua | awk '{print $1}')
 
-
 if [ -n "$PACKAGES" ]; then
 	echo -e "Packages :\n" && echo -e "$PACKAGES\n"
 fi
 
-
 if [ -n "$AURPACKAGES" ]; then
 	echo -e "AUR Packages :\n" && echo -e "$AURPACKAGES\n"
 fi
-
 
 if [ -z "$PACKAGES" ] && [ -z "$AURPACKAGES" ];then
 	echo -e "No update available\n" && read -n 1 -r -s -p $'Press \"enter\" to quit\n'
 else
 	read -n 1 -r -s -p $'Press \"enter\" to apply updates or \"ctrl + c\" to quit\n'
 	sudo pacman -Syu && yay -Syu
+	echo -e "\nUpdates have been applied\n" && read -n 1 -r -s -p $'Press \"enter\" to quit\n'
 fi
