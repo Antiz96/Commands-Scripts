12b183cb67b70767bedf1e5dac8dea5392377a06
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-11-08 16:54:17 +0100

    Add files via upload

---
+++ genmon_update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "<icon>mintupdate-checking</icon>"
+echo "<iconclick>xfce4-terminal -x /home/rcandau/Documents/scripts/check_update.sh</iconclick>"
+echo "<txt>$(/home/rcandau/Documents/scripts/update_notifier.sh)</txt>"
