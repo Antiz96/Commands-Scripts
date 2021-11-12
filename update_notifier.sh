facd7b2cceb419c339804c9300c2600d1ab9885a
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2021-11-12 18:24:14 +0100

    Update update_notifier.sh

--- update_notifier.sh
+++ update_notifier.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-UPDATE_NUMBER=$( (/usr/bin/checkupdates && /usr/bin/yay -Qua) | wc -l)
+UPDATE_NUMBER=$( (/usr/bin/checkupdates ; /usr/bin/yay -Qua) | wc -l)
 
 if [ "$UPDATE_NUMBER" -ne 0 ]; then
         echo "$UPDATE_NUMBER"
