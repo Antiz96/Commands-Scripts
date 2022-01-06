c01270d38b00fb2df3e2f1cd51cc8f38cac38c3e
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2022-01-06 20:31:46 +0100

    Update install_bluetooth_driver.sh

--- install_bluetooth_driver.sh
+++ install_bluetooth_driver.sh
@@ -7,7 +7,6 @@ install=$(cd /run/media/rcandau/data/bluetooth/bt_usb/ && sudo make install > /d
 
 if [ "$install" = "true" ]; then
         read -n 1 -r -s -p $'Please, plug the USB bluetooth dongle, then press enter to continue\n'
-        echo ""
         sleep 3
 
         hci=$(hciconfig -a | grep -c "24:4B:FE:3A:1A:FD")
@@ -16,11 +15,10 @@ if [ "$install" = "true" ]; then
                 exit 0
         elif [ "$hci" = 0 ]; then
                 echo "USB bluetooth dongle not recognized, something went wrong"
-                echo "Please, try to relaunch the script and make sure you followed each steps"
+                echo "Please, try to relaunch the script and make sure you followed each steps correctly"
                 exit 1
         else
-                echo "Something went wrong, please check the following return"
-                echo ""
+                echo -e "Something went wrong, please check the following return\n"
                 hciconfig -a
                 exit 1
         fi
