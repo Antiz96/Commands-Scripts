ca5fd4ea95fdafbf098519b0f9e53c5f1f7e3a27
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2023-05-18 16:32:19 +0200

    Delete genmon_update.sh

--- genmon_update.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "<icon>mintupdate-checking</icon>"
-echo "<iconclick>xfce4-terminal -x /home/rcandau/Documents/scripts/check_update.sh</iconclick>"
-echo "<txt>$(/home/rcandau/Documents/scripts/update_notifier.sh)</txt>"
