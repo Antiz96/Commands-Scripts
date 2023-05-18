3f61aed4d828689d4f257f0049601a5adbd9d71d
Robin Candau <53110319+Antiz96@users.noreply.github.com>
2023-05-18 16:20:06 +0200

    Delete update_notifier.sh

--- update_notifier.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-UPDATE_NUMBER=$( (/usr/bin/checkupdates ; /usr/bin/yay -Qua) | wc -l)
-
-if [ "$UPDATE_NUMBER" -ne 0 ]; then
-        echo "$UPDATE_NUMBER"
-else
-        echo ""
-fi
