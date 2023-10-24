269e73dbf7ce43b7255684507f0ec4ede4d8960a
Robin Candau <robincandau@protonmail.com>
2023-10-24 14:33:28 +0200

    Typo + change var name for logfile

--- monitor-servers-wakeonlan.sh
+++ monitor-servers-wakeonlan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is a script I made which run on a raspberry PI to monitor my physical servers' responsiveness and send them a wakeonlan packet after a certain period of inactivity (useful to power them back on automatically after a power outage for instance)
+# This is a script I made which runs on a raspberry PI to monitor my physical servers' responsiveness and send them a wakeonlan packet after a certain period of inactivity (useful to power them back on automatically after a power outage for instance)
 
 declare -A servers
 servers["pmx01.rc"]="7c:10:c9:8c:88:9d"
@@ -8,7 +8,7 @@ servers["pmx02.rc"]="68:1d:ef:30:cc:88"
 
 declare -A fail_counter
 
-log_file="/var/log/monitor-servers-wakeonlan/wol_packet.log"
+logfile="/var/log/monitor-servers-wakeonlan/wol_packet.log"
 
 while true; do
         for server in "${!servers[@]}"; do
@@ -19,7 +19,7 @@ while true; do
                         fail_counter["${server}"]=$((fail_counter["${server}"] + 1))
                         echo "${server} fail counter: ${fail_counter["${server}"]}"
                         if [ "${fail_counter["${server}"]}" -eq 6 ]; then
-                                wakeonlan "${servers["${server}"]}" && echo "$(date) - Wake On Lan packet sent to ${server}" >> "${log_file}" || echo "$(date) - Error sending a Wake On Lan packet to ${server}" >> "${log_file}"
+                                wakeonlan "${servers["${server}"]}" && echo "$(date) - Wake On Lan packet sent to ${server}" >> "${logfile}" || echo "$(date) - Error sending a Wake On Lan packet to ${server}" >> "${logfile}"
                                 fail_counter["${server}"]=$((fail_counter["${server}"] - 1))
                         fi
                 fi
