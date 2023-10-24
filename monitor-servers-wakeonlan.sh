958c0271ce55079efacb2a32f78a8d2e2ac8a080
Robin Candau <robincandau@protonmail.com>
2023-10-24 16:17:20 +0200

    Phrasing

--- monitor-servers-wakeonlan.sh
+++ monitor-servers-wakeonlan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is a script I made which runs on a raspberry PI to monitor my physical servers' responsiveness and send them a wakeonlan packet after a certain period of inactivity (useful to power them back on automatically after a power outage for instance)
+# This is a script I made which runs on a raspberry PI to monitor my physical servers' responsiveness and send them a wakeonlan packet after a certain period of downtime (useful to power them back on automatically after a power outage for instance)
 
 declare -A servers
 servers["pmx01.rc"]="7c:10:c9:8c:88:9d"
